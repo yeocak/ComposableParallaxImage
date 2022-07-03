a9d803e1044c515decffe2f8a7a54d1bc07f5916
yeocak <ocakyunusemre@hotmail.com>
2022-07-03 22:56:33 +0300

    Testing git

--- app/src/main/java/com/yeocak/composableparallaximage/MainActivity.kt
+++ app/src/main/java/com/yeocak/composableparallaximage/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting()
-
                 }
             }
         }
